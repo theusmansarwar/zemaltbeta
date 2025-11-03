31beb1c11cf1ffdb88d4f171cfe699748c174a25
Aleem Ahmad <ahmadaleem592@gmail.com>
2025-11-03 12:22:44 +0500

    updated files

--- src/Components/ServicePageFeatured/ServicePageFeatured.jsx
+++ src/Components/ServicePageFeatured/ServicePageFeatured.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./ServicePageFeatured.css";
 const ServicePageFeatured = ({ data }) => {
   return (
-    <div className="DesigningCardFeatured">
+    <div className="servicepage-featured">
       <h1>
         {data.title} <span>{data.spanTitle}</span>
       </h1>
-      <p>{data.description}</p>
+      <p className="featured-desc">{data.description}</p>
     </div>
   );
 };
