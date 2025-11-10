90a842e4b3d2a5b3d823162a5a0832d085ee45a4
Aleem Ahmad <ahmadaleem592@gmail.com>
2025-11-10 17:48:17 +0500

    updated files

--- src/app/layout.js
+++ src/app/layout.js
@@ -55,7 +55,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="robots" content="noindex, nofollow" />
         <link
           href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap"
           rel="stylesheet"
