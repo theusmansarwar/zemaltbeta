4220de6cc5dedb556e8615aa64fbf1a628d2e5f3
Aleem Ahmad <ahmadaleem592@gmail.com>
2025-11-28 16:34:41 +0500

    updated files

--- src/Components/BlogDetail/LeftDetail.jsx
+++ src/Components/BlogDetail/LeftDetail.jsx
@@ -11,7 +11,7 @@ const LeftDetail = ({ blog }) => {
       <img className="blog-detail-thumbnail" src={baseUrl + blog.thumbnail} alt={blog.title} />
       <div
         dangerouslySetInnerHTML={{ __html: blog?.detail }}
-        className="custom-html-styles px-4 sm:px-8 mt-6 text-white  dangerously-set-data  "
+        className="custom-html-styles px-4 sm:px-8 mt-6 text-white  dangerously-set-data"
       ></div>
     </div>
   );
