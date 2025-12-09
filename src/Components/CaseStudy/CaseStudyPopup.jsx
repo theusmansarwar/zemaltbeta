88fcc87afd0b6b62ef408d072f0c29b83aa84e67
Aleem Ahmad <ahmadaleem592@gmail.com>
2025-12-09 13:09:13 +0500

    updated files

--- src/Components/CaseStudy/CaseStudyPopup.jsx
+++ src/Components/CaseStudy/CaseStudyPopup.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from "react";
 import "./CaseStudyPopup.css";
 import { IoCloseCircleSharp } from "react-icons/io5";
--- src/Components/Realworld/RealWorldPopup.jsx
+++ src/Components/Realworld/RealWorldPopup.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import "./RealWorldPopup.css";
 import { IoCloseCircleSharp } from "react-icons/io5";
@@ -6,62 +7,63 @@ import { baseUrl } from "@/config/Config";
 import { toast } from "react-toastify";
 
 const RealWorldPopup = ({ onClose, project }) => {
-    const handleDownload = async () => {
-        try {
-            const response = await fetch(baseUrl + project.file, {
-                method: "GET",
-            });
+  const handleDownload = async () => {
+    try {
+      const response = await fetch(baseUrl + project.file, {
+        method: "GET",
+      });
 
-            if (!response.ok) {
-                throw new Error("Failed to download file");
-            }
+      if (!response.ok) {
+        throw new Error("Failed to download file");
+      }
 
-            const blob = await response.blob();
-            const url = window.URL.createObjectURL(blob);
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
 
-            const link = document.createElement("a");
-            link.href = url;
-            link.download = project.name ? `${project.name}.pdf` : "file.pdf";
-            document.body.appendChild(link);
-            link.click();
-            link.remove();
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = project.name ? `${project.name}.pdf` : "file.pdf";
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
 
-            // Cleanup blob URL
-            window.URL.revokeObjectURL(url);
-        } catch (error) {
-            toast.error("Download error:", error);
-        }
-    };
+      // Cleanup blob URL
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      toast.error("Download error:", error);
+    }
+  };
 
-    return (
-        <div className="rw-popup-parent">
-            <div className="rw-popup">
-                <IoCloseCircleSharp className="close-icon" onClick={onClose} />
-                <div className="top">
-                    <div
-                        className="blur-bg"
-                        style={{ backgroundImage: `url(${baseUrl + project.image})` }}
-                    ></div>
-                    <img src={baseUrl + project.image} alt={project.name} />
-                </div>
-
-                <div className="bottom">
-                    <h2>{project.name}</h2>
-                    {project.detail && (
-                        <div className="dangerously-set-data"
-                            dangerouslySetInnerHTML={{
-                                __html: project.detail,
-                            }}
-                        />
-                    )}
-
-                    <button className="proposal-btn" onClick={handleDownload}>
-                        <span>Download PDF</span> <FaArrowDown />
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="rw-popup-parent">
+      <div className="rw-popup">
+        <IoCloseCircleSharp className="close-icon" onClick={onClose} />
+        <div className="top">
+          <div
+            className="blur-bg"
+            style={{ backgroundImage: `url(${baseUrl + project.image})` }}
+          ></div>
+          <img src={baseUrl + project.image} alt={project.name} />
         </div>
-    );
+
+        <div className="bottom">
+          <h2>{project.name}</h2>
+          {project.detail && (
+            <div
+              className="dangerously-set-data"
+              dangerouslySetInnerHTML={{
+                __html: project.detail,
+              }}
+            />
+          )}
+
+          <button className="proposal-btn" onClick={handleDownload}>
+            <span>Download PDF</span> <FaArrowDown />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RealWorldPopup;
